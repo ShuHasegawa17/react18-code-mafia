8f60d2ed38dfacf5d35febb46acf93d2fb119b40
shuhasegawa <shuhasegawa0430@gmail.com>
2023-07-03 23:14:36 +0900

    useContext

--- 11_hooks_p1/src/050_context_file/start/components/Header.js
+++ 11_hooks_p1/src/050_context_file/start/components/Header.js
@@ -1,5 +1,26 @@
+import { useTheme } from '../context/ThemeContext';
+
 const Header = () => {
-  
+  const THEMES = ['light', 'dark', 'red'];
+  const [theme, setTheme] = useTheme();
+
+  const changeTheme = (e) => setTheme(e.target.value);
+  return (
+    <header className={`content-${theme}`}>
+      {THEMES.map((t) => (
+        <label key={t}>
+          <input
+            type="radio"
+            name="theme"
+            value={t}
+            checked={t === theme}
+            onChange={changeTheme}
+          />
+          {t}
+        </label>
+      ))}
+    </header>
+  );
 };
 
 export default Header;
